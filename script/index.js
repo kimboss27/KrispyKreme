570f9506aabf56f62259bbe8f0634450ce3578bf
kimboss27 <kimboss27@naver.com>
2023-01-27 10:24:09 +0900

    krispy kreme

--- script/index.js
+++ script/index.js
@@ -38,11 +38,11 @@ $(document).ready(function(){
 
     /* --- slide banner --- */
     const swiper = new Swiper('.swiper', {
-        loop: true,
-        autoplay: {
-            delay: 2500,
-            disableOnInteraction: false,
-          },
+        
+        pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        },
       });
 
     /* --- news --- */
